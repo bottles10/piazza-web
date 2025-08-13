a40b7f88a0b87e69364fc60543c64ceea8970c4c
bottles10 <henryaovare@gmail.com>
2025-08-13 18:36:15 -0500

    Build the authentication system

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,15 +1,28 @@
 class User < ApplicationRecord
-    has_many :memberships, dependent: :destroy
-    has_many :organizations, through: :memberships
+	has_many :memberships, dependent: :destroy
+	has_many :organizations, through: :memberships
+	has_many :app_sessions
 
-    has_secure_password
+	has_secure_password
 
-    validates :name, presence: true
-    validates :email, 
-        format: { with: URI::MailTo::EMAIL_REGEXP },
-        uniqueness: { case_sensitive: false }
-    validates :password, presence: true, length: { minimum: 8 }
+	validates :name, presence: true
+	validates :email, 
+			format: { with: URI::MailTo::EMAIL_REGEXP },
+			uniqueness: { case_sensitive: false }
+	validates :password, presence: true, length: { minimum: 8 }
 
-    normalizes :name, with: -> (name) { name.strip.capitalize }
-    normalizes :email, with: -> (email) { email.strip.downcase }
+	normalizes :name, with: -> (name) { name.strip.capitalize }
+	normalizes :email, with: -> (email) { email.strip.downcase }
+
+
+	def self.create_app_session(email:, password:)
+		user = User.authenticate_by(email: email, password: password)
+		user.app_sessions.create if user.present?
+	end
+
+	def authenticate_app_session(app_session_id, token)
+		app_sessions.find(app_session_id).authenticate_token(token)
+	rescue ActiveRecord::RecordNotFound
+		nil
+	end
 end
